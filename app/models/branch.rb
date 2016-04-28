9e705ab72d3f7ccbbe7d232b4f92f4c8ddc49079
SwatGetmann <swatgetmann@gmail.com>
2016-04-28 07:54:13 +0300

    [BSU-CP] provide has many associations for user, branch and commit models

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -3,6 +3,9 @@ class Branch < ActiveRecord::Base
   has_one :workspace, inverse_of: :branch
   has_many :commits, inverse_of: :branch
 
+  has_many :branch_members
+  has_many :contributors, through: :branch_members, source: :user
+
   after_create :create_initial_commit
   after_create :create_workspace
 
